8b510e0a13ef559a83512e4c1bcd4f019b7b3691
xzllzx <73070369+xzllzx@users.noreply.github.com>
2024-04-19 12:23:09 +0800

    Initialize Spring Boot app

---
+++ src/main/java/com/example/javaloginwithemail/JavaLoginWithEmailApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaloginwithemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLoginWithEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLoginWithEmailApplication.class, args);
+    }
+
+}
